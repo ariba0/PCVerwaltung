521cf037c02c3e1e78f61b7f6efbc0c6769c71ab
ariba0 <105052973+ariba0@users.noreply.github.com>
2022-09-27 11:56:50 +0200

    Kunde fix

--- PCVerwaltung/PCVerwaltung/Model/Kunde.cs
+++ PCVerwaltung/PCVerwaltung/Model/Kunde.cs
@@ -8,5 +8,8 @@ namespace PCVerwaltung.Model
 {
     class Kunde
     {
+        private string _name;
+        public string Name { get; set; }
+        private List<Rechnung> _rechnungen;
     }
 }
