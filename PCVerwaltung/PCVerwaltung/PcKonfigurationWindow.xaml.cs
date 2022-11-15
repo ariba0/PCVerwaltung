6673011fd2fd802cdf319b80a7355d507f6bf228
GilCaetano <101702065+GilCaetano@users.noreply.github.com>
2022-11-15 10:19:42 +0100

    xaml anpassungen
    added PcKonfigurationWindow.xaml

---
+++ PCVerwaltung/PCVerwaltung/PcKonfigurationWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PCVerwaltung
+{
+    /// <summary>
+    /// Interaktionslogik für PcKonfigurationWindow.xaml
+    /// </summary>
+    public partial class PcKonfigurationWindow : Window
+    {
+        public PcKonfigurationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CpuComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
