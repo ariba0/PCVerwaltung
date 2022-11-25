aa1a085064f998217a5ed2f6f384cc2cb4f66922
nastpatrick <patrick.nast@its-stuttgart.de>
2022-11-25 09:11:29 +0100

    Klassen mit neuen Attributen erweitern

--- PCVerwaltung/PCVerwaltung/Model/HWKomponente/SSD.cs
+++ PCVerwaltung/PCVerwaltung/Model/HWKomponente/SSD.cs
@@ -9,10 +9,14 @@ namespace PCVerwaltung.Model.HWKomponente
     class SSD : HWKomponent
     {
         private int _speicher;
+        private int _lesegeschwindigkeit;
+        private int _schreibgeschwindigkeit;
         public string Bezeichner { get => _bezeichner; set => _bezeichner = value; }
-        public SSD(string bezeichner, float preis, string hersteller, int speicher) : base(bezeichner, preis, hersteller)
+        public SSD(string bezeichner, float preis, string hersteller, int speicher, int lesegeschwindigkeit, int schreibgeschwindigkeit) : base(bezeichner, preis, hersteller)
         {
             _speicher = speicher;
+            _lesegeschwindigkeit = lesegeschwindigkeit;
+            _schreibgeschwindigkeit = schreibgeschwindigkeit;
         }
     }
 }
